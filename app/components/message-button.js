4ee3f7e82972ad8dc995b003692c5b506fc21a2d
Adam Bodie <aduo3000@yahoo.com>
2017-05-05 17:20:31 -0700

    Added message-button component

---
+++ app/components/message-button.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    actions: {
+        confirmItems(){
+            alert('Great, now we are ready to go!!!')
+        }
+    }
+});

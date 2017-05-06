b3d11e59c5eda9a19cada5e41c656e27ef2592e7
Adam Bodie <aduo3000@yahoo.com>
2017-05-05 18:31:51 -0700

    Added language-list component, removed hide.js

--- app/routes/one.js
+++ app/routes/one.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(){
+ return[
+        {'language': 'English', 'name': 'Grand Canyon', 'id': 0},
+        {'language': 'Hopi', 'name': 'Ongtupqa', 'id': 1},
+        {'language': 'Yavapai', 'name': 'Wi:kaʼi:la', 'id': 2},
+        {'language': 'Spanish', 'name': 'Gran Cañón', 'id': 3}
+        ]
+    }
 });
